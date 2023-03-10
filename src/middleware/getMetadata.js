315c057b9191eb3f6d1def3f12745e24006af49f
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2023-03-10 19:13:54 +0100

    Add metadata from db

--- src/middleware/getMetadata.js
+++ src/middleware/getMetadata.js
@@ -1,27 +1,30 @@
 const glob = require('glob')
 const globParent = require('glob-parent')
 
-
-// Create dict with all media files and their respective metadata
-  const media = glob.sync(__mediaPath + '/*('+ __mediaFolders.toString().replaceAll(',', '|') + ')/*')
-    .map(path => {
-      const tmp = globParent(path)
-      const type = tmp.substring(tmp.lastIndexOf('/') + 1, tmp.length)
-      const viewer = type == 'pano' ? 'pano' : 'img'
-      const id = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.'))
-      return {
-        type: type,
-        viewer: viewer,
-        file: id
-      }
-    })
-
-module.exports = media.reverse()
-
 async function main() {
   // Get all the metadata on the db
   const docs = await __Island.find({})
-  //console.log(docs)
+
+  // Create dict with all media files and their respective metadata
+    const media = glob.sync(__mediaPath + '/*('+ __mediaFolders.toString().replaceAll(',', '|') + ')/*')
+      .map(path => {
+        const tmp = globParent(path)
+        const type = tmp.substring(tmp.lastIndexOf('/') + 1, tmp.length)
+        const viewer = type == 'pano' ? 'pano' : 'img'
+        const id = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.'))
+        console.log("----------------------------------")
+        const dbMetadata = docs.filter(i => i.name === id)
+        console.log(c)
+        
+        
+        return {
+          type: type,
+          viewer: viewer,
+          file: id
+        }
+      })
+
+  module.exports = media.reverse()
 }
 
 main()
