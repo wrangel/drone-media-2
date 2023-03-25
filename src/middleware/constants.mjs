f6b3ae8712f1e4ed8099d678236548576f6fcfda
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2023-03-25 17:21:41 +0100

    finish url grabber

--- src/middleware/constants.mjs
+++ src/middleware/constants.mjs
@@ -8,13 +8,21 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
+    // AWS
+    BUCKET_NAME: process.env.BUCKET_NAME,
+    BUCKET_REGION: process.env.BUCKET_REGION,
+    ACCESS_KEY: process.env.ACCESS_KEY,    
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+    EXPIRY_TIME: 86400, // in seconds. Here: a day
+    // MongoDB
+    DB: 'ellesmereDB',
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    // Misc
     ACCESS_TOKEN: process.env.ACCESS_TOKEN,
     AUTHOR_PICTURES_PATH: 'media/author_pictures/',
     AUTHOR_PICTURE_FORMAT: '.svg',
     ADDRESS_COMPONENTS: ["address", "postcode", "place", "region", "country"],
-    DB: 'ellesmereDB',
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
     MEDIA_FOLDERS: ['hdr', 'pan', 'wide_angle'], // MUST be sorted alphabetically
     PANO_FIRST_IMAGE: 'DJI_0001',
     PORT: 8080,
--- src/middleware/getSignedUrl.mjs
+++ src/middleware/getSignedUrl.mjs
@@ -1,52 +1,42 @@
-import { S3Client, PutObjectCommand, GetObjectCommand, ListObjectsCommand} from '@aws-sdk/client-s3'
+import { S3Client, GetObjectCommand, ListObjectsCommand} from '@aws-sdk/client-s3'
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
-import dotenv from 'dotenv'
-dotenv.config()
-
-const bucketName = process.env.BUCKET_NAME
-const bucketRegion = process.env.BUCKET_REGION
-const accessKey = process.env.ACCESS_KEY
-const secretAccessKey = process.env.SECRET_ACCESS_KEY
+import Constants from '../middleware/constants.mjs'
 
 // Get image identifyer from image path
 const getId = path => {
   return path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.'))
 }
 
-/*
-
 // Create instance of S3 Client
 const s3 = new S3Client({
   credentials: {
-    accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey
+    accessKeyId: Constants.ACCESS_KEY,
+    secretAccessKey: Constants.SECRET_ACCESS_KEY
   },
-  region: bucketRegion
+  region: Constants.BUCKET_REGION
 })
 
-async function main() {
+// Get the urls
+async function getUrls() {
+  // Get all the files in the bucket
+  const list = await s3.send(new ListObjectsCommand( { Bucket: Constants.BUCKET_NAME } ))
 
-    const list = await s3.send(new ListObjectsCommand( { Bucket: bucketName } ))
-    //console.log(list.Contents)
-    
-    // Pan out all the info
-    const arr = await Promise.all(
-      list.Contents.map(async content => {
-        const key = content.Key
-        const id = getId(key)
-        const parent = key.substring(0, key.indexOf('/'))
-        const type = parent == 'thumbnails' ? 'thumbnails' : 'actual'
-        return {
-          id: id,
-          type, 
-          sigUrl: await getSignedUrl(s3, new GetObjectCommand({ Bucket: bucketName,  Key: key }, { expiresIn: 86400 } )) // in seconds. Here: a day
-        }
-      })
-    )
+  // Get presigned url
+  const arr = await Promise.all(
+    list.Contents.map(async content => {
+      const key = content.Key
+      const type = key.substring(0, key.indexOf('/')) == 'thumbnails' ? 'thumbnails' : 'actual'
+      return {
+        id: getId(key),
+        type, 
+        sigUrl: await getSignedUrl(
+          s3, new GetObjectCommand({ Bucket: Constants.BUCKET_NAME,  Key: key }, { expiresIn: Constants.EXPIRY_TIME } )
+        )
+      }
+    })
+  )
 
-//console.log(arr)
-  
-  // Reduce on the id
+  // Reduce the info on the id
   const result = arr.reduce((acc, d) => {
     const found = acc.find(a => a.id === d.id)
     const value = { type: d.type, sigUrl: d.sigUrl } // the element in data property
@@ -60,11 +50,9 @@ async function main() {
   }, 
   [])
   
-  console.log(result)
-
   // sort urls based on type (actual, thumbnail)
-  let sortedResult = result.map(r => {
-    let sorted = r.data.sort(function (a, b) {
+  const sortedResult = result.map(r => {
+    const sorted = r.data.sort(function (a, b) {
       if (a.type < b.type) {
         return -1;
       }
@@ -73,25 +61,16 @@ async function main() {
       }
       return 0;
     })
-
     return {id: r.id, data: sorted}
   })
 
-  // Create new kv pair
-  let finalResult = sortedResult.map(elem => {
-    
-    let values_0 = Object.values(elem.data)[0]
-    let values_1 = Object.values(elem.data)[1]
-    //let a00 = Object.values(values_0)[0]
-    let a01 = Object.values(values_0)[1]
-    //let a10 = Object.values(values_1)[0]
-    let a11 = Object.values(values_1)[1]
-
-    const d = new URL(a01).toJSON()
-    const e = new URL(a11).toJSON()
-
-    return {name: elem.id, urls: {actual: a01, thumbnail: a11}}
+  // Create new key value pair
+  const finalResult = sortedResult.map(elem => {
+    let actual_url = Object.values(Object.values(elem.data)[0])[1]
+    let thumbnail_url = Object.values(Object.values(elem.data)[1])[1]
+    return {name: elem.id, urls: {actual: actual_url, thumbnail: thumbnail_url}}
   })
 
-  */
- 
+}
+
+getUrls()
