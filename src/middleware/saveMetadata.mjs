c9c3503cfe48f3dd3ef958fed60e61be1c53f163
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2023-03-28 20:36:26 +0200

    Fix altitude

--- src/middleware/saveMetadata.mjs
+++ src/middleware/saveMetadata.mjs
@@ -5,42 +5,56 @@ import { Island } from './manageConnections.mjs'
 /*  Converts the timestamp string into a GMT / Local date (that is what exifr is doing wrong!)
     https://stackoverflow.com/questions/43083993/javascript-how-to-convert-exif-date-time-data-to-timestamp
 */
-const getDate = s => { 
-    const [year, month, date, hour, min, sec] = s.split(/\D/) 
+const getDate = str => { 
+    const [year, month, date, hour, min, sec] = str.split(/\D/) 
     return new Date(year, month - 1 ,date, hour, min, sec) 
 }
 
-// Convert GPS in case string is returned
-const convertGPS = coord => {
-    let corrected
-    try {
-        corrected = parseFloat(coord)
-    } catch {
-        corrected = coord.match(/[0-9]/g)
+// Converts the altitude into meter-above-sea
+const getAltitude = altitudeString => {
+    let altitude
+    if (altitudeString.endsWith('m')) {
+        altitude = parseFloat(altitudeString.replace('m', ''))
+    } else {
+        const components = altitudeString.split('/').map(component => parseFloat(component))
+        altitude = components[0] / components[1]
     }
-    return corrected
+    return altitude
+}
+
+// Convert GPS, if string is returned
+const getCoordinates = coordString => {
+    let coord
+    try {
+        coord = parseFloat(coordString)
+    } catch {
+        coord = coordString.match(/[0-9]/g)
+    }
+    return coord
 }
 
 // Save the data to the db
 async function save(media) {
 
     // Get exif data for the new files
-    const exifData = await Promise.all( 
+    const base = await Promise.all( 
         media.map(async medium => {
             const exif = await ExifReader.load(medium.sigUrl) // Slow, but reliable (exifr is fast, but omits timezone offset)
             return {
                     key: medium.key,
                     path: medium.path,
                     exif_datetime: exif.DateTimeOriginal.description,
-                    exif_longitude: convertGPS(exif.GPSLongitude.description),
-                    exif_latitude: convertGPS(exif.GPSLatitude.description),
-                    exif_altitude: exif.GPSAltitude.description
+                    exif_longitude: getCoordinates(exif.GPSLongitude.description),
+                    exif_latitude: getCoordinates(exif.GPSLatitude.description),
+                    exif_altitude: getAltitude(exif.GPSAltitude.description)
             }
         })
     )
 
+        console.log(base)
+
     // Get the urls for the reverse engineering call
-    const reverseUrls = exifData.map (
+    const reverseUrls = base.map (
         exif => Constants.REVERSE_GEO_URL_ELEMENTS[0] + exif.exif_longitude + ', ' + exif.exif_latitude + 
                     Constants.REVERSE_GEO_URL_ELEMENTS[1] + Constants.REVERSE_GEO_ACCESS_TOKEN 
     )
@@ -76,12 +90,25 @@ async function save(media) {
     }
 
     // Combine everything into the Mongoose compatible metadata
+    const combined = base.map(function (x, i) {
+        //console.log(x, reverseGeocodingData[i])
+    })
+
+     /*
     const combined = reverseGeocodingData.map(function (reverse, i) {
         const base = media[i]
         const exif = exifData[i]
-        console.log(base, exif, reverse)
+        metadata.name = base.key
+        metadata.type = base.path.substring(0, base.path.indexOf('/'))
+        metadata.author = ''
+        metadata.dateTimeString = exif.exif_datetime
+        metadata.dateTime = getDate(exif.exif_datetime)
+        metadata.geometry.type = 'Point'
+        metadata.geometry.coordinates.latitude = exif.exif_latitude
+        metadata.geometry.coordinates.longitude = exif.exif_longitude
     })
-
+*/
+    //console.log(metadata)
 
 
 }
@@ -90,18 +117,9 @@ export { save }
 
     /*
 
-    // Combine everything into the Mongoose compatible metadata
-    const combined = reverseData.map(function (reverse, i) {
-        const base = media[i]
-        const exif = exifdata[i]
-        metadata.name = base.key
-        metadata.type = base.folder
-        metadata.author = ''
-        metadata.dateTimeString = exif.DateTimeOriginal.description
-        metadata.dateTime = getDate(exif.DateTimeOriginal.description)
-        metadata.geometry.type = 'Point'
-        metadata.geometry.coordinates.latitude = exif.GPSLatitude.description
-        metadata.geometry.coordinates.longitude = exif.GPSLongitude.description
+        
+        
+
         metadata.altitude = parseFloat(exif.GPSAltitude.description.replace(' m', ''))
         metadata.country = reverse.country
         metadata.region = reverse.region
