e7b50fc7a10c9815755589130a3c82192d876e3a
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2023-03-29 21:17:39 +0200

    Unify indentation2

--- src/middleware/updateFiles.mjs
+++ src/middleware/updateFiles.mjs
@@ -21,33 +21,38 @@ const update = media => {
         // Determine if image needs compression and / or resizing
         const compressFlag = target.indexOf(Constants.THUMBNAIL_FOLDER) > -1 ? true : false
         const resizeFlag = compressFlag && medium.type  != 'hdr' ? true : false
-          /*  Create a PassThrough Stream and an upload container
-          Thanks, @danalloway, https://github.com/lovell/sharp/issues/3313, https://sharp.pixelplumbing.com/api-constructor
-          */
-          const uploadStream = new PassThrough()
-          const upload = new Upload({
-            client: s3,
-            queueSize: 1,
-            params: {
-                Bucket: Constants.SITE_BUCKET,
-                ContentType: `image/${Constants.SITE_MEDIA_FORMAT}`,
-                Key: target,
-                Body: uploadStream
-            },
-          })
 
-          // Create Sharp transformation stream
-          const transformStream = sharp()
-            .webp( { lossless: compressFlag } )
-          if(resizeFlag) {
-            transformStream.resize({
-                width: 2000,
-                height: 1300,
-                position: sharp.strategy.attention
-            })
-          }
-          response.pipe(transformStream).pipe(uploadStream)
-          //return await upload.done()
+
+
+
+
+
+        /*  Create a PassThrough Stream and an upload container
+        Thanks, @danalloway, https://github.com/lovell/sharp/issues/3313, https://sharp.pixelplumbing.com/api-constructor
+        */
+        const uploadStream = new PassThrough()
+        const upload = new Upload({
+          client: s3,
+          queueSize: 1,
+          params: {
+              Bucket: Constants.SITE_BUCKET,
+              ContentType: `image/${Constants.SITE_MEDIA_FORMAT}`,
+              Key: target,
+              Body: uploadStream
+          },
+        })
+        // Create Sharp transformation stream
+        const transformStream = sharp()
+          .webp( { lossless: compressFlag } )
+        if(resizeFlag) {
+          transformStream.resize({
+              width: 2000,
+              height: 1300,
+              position: sharp.strategy.attention
+          })
+        }
+        response.pipe(transformStream).pipe(uploadStream)
+        //return await upload.done()
       })
     })
   )
