132a12c7e6ea080a62ea40a70e7ddc802ffe7007
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2023-04-12 13:50:53 +0200

    Add if switch to helper function

--- helper.mjs
+++ helper.mjs
@@ -21,54 +21,61 @@ const fileInfo = files
         .replace(Constants.RENAME_IDS[0], Constants.REPLACEMENT)
     }
     return {name: name, sourceFile: sourceFile, targetFile: name + Constants.MEDIA_FORMATS.large}
-  })  
-
-// Collect user input about authors of the media (while is async by nature!)
-let idx = 0
-while (idx < fileInfo.length) {
-  const name = fileInfo[idx].name
-  const answer = await question(`Author and media type of --> ${ name } <-- (comma separated) : `)
-  let [author, mediaType] = answer.split(',').map(x => x.trim())
-  // Allow only known authors
-  while (!Constants.CONTRIBUTORS.includes(author)) {
-    author = await question(`Please choose one of (${ Constants.CONTRIBUTORS }) as author for --> ${ name }  <-- : `)
-  }
-  // Allow only known media types
-  while (!Constants.MEDIA_PAGES.includes(mediaType)) {
-    mediaType = await question(`Please choose one of (${ Constants.MEDIA_PAGES }) as media type for --> ${ name } <-- : `)
-  }
-  // Add the new info to the fileInfo object
-  fileInfo[idx].author = author
-  fileInfo[idx].mediaType = mediaType
-  idx += 1
-}
-
-/// B) Update MongoDB
-// Prepare JSON for MongoDB insert
-const newIslands = fileInfo.map(element => { return { name: element.name, type: element.mediaType, author: element.author } })
-// Update MongoDB
-await Island.insertMany(newIslands)
-
-/// C) Upload media to AWS S3 (requires AWS CLI with proper authentication: Alternative would be an S3 client)
-fileInfo.forEach(fi => 
-  runCli(`aws s3 cp ${process.env.INPUT_DIRECTORY}${fi.sourceFile} s3://${process.env.ORIGINALS_BUCKET}/${fi.mediaType}/${fi.targetFile}`)
-)
-
-/// D) Convert file to .jpeg, copy .jpeg to OneDrive, move .tif to 'done' folder
-await Promise.all(
-  fileInfo.map(async fi => {
-    const inputFile = path.join(process.env.INPUT_DIRECTORY, fi.sourceFile)
-    // Handle jpegs
-    await sharp(inputFile)
-      .jpeg({ quality: 100 })
-      .toFile(path.join(process.env.ONEDRIVE_DIRECTORY, fi.sourceFile.replace(Constants.MEDIA_FORMATS.large, Constants.MEDIA_FORMATS.small)))
-    // Handle .tifs
-    fs.rename(inputFile, path.join(process.env.OUTPUT_DIRECTORY, fi.sourceFile), function (err) {
-      if (err) {
-          throw err
-      } else {
-          console.log(`Successfully moved ${inputFile}`)
-      }
-    })
   })
-)
+
+const noMedia = fileInfo.length
+if (noMedia == 0) {
+  console.log("No media to manage")
+} 
+else {
+  console.log(`${noMedia} media to manage`)
+  // Collect user input about authors of the media (while is async by nature!)
+  let idx = 0
+  while (idx < fileInfo.length) {
+    const name = fileInfo[idx].name
+    const answer = await question(`Author and media type of --> ${ name } <-- (comma separated) : `)
+    let [author, mediaType] = answer.split(',').map(x => x.trim())
+    // Allow only known authors
+    while (!Constants.CONTRIBUTORS.includes(author)) {
+      author = await question(`Please choose one of (${ Constants.CONTRIBUTORS }) as author for --> ${ name }  <-- : `)
+    }
+    // Allow only known media types
+    while (!Constants.MEDIA_PAGES.includes(mediaType)) {
+      mediaType = await question(`Please choose one of (${ Constants.MEDIA_PAGES }) as media type for --> ${ name } <-- : `)
+    }
+    // Add the new info to the fileInfo object
+    fileInfo[idx].author = author
+    fileInfo[idx].mediaType = mediaType
+    idx += 1
+  }
+
+  /// B) Update MongoDB
+  // Prepare JSON for MongoDB insert
+  const newIslands = fileInfo.map(element => { return { name: element.name, type: element.mediaType, author: element.author } })
+  // Update MongoDB
+  await Island.insertMany(newIslands)
+
+  /// C) Upload media to AWS S3 (requires AWS CLI with proper authentication: Alternative would be an S3 client)
+  fileInfo.forEach(fi => 
+    runCli(`aws s3 cp ${process.env.INPUT_DIRECTORY}${fi.sourceFile} s3://${process.env.ORIGINALS_BUCKET}/${fi.mediaType}/${fi.targetFile}`)
+  )
+
+  /// D) Convert file to .jpeg, copy .jpeg to OneDrive, move .tif to 'done' folder
+  await Promise.all(
+    fileInfo.map(async fi => {
+      const inputFile = path.join(process.env.INPUT_DIRECTORY, fi.sourceFile)
+      // Handle jpegs
+      await sharp(inputFile)
+        .jpeg({ quality: 100 })
+        .toFile(path.join(process.env.ONEDRIVE_DIRECTORY, fi.sourceFile.replace(Constants.MEDIA_FORMATS.large, Constants.MEDIA_FORMATS.small)))
+      // Handle .tifs
+      fs.rename(inputFile, path.join(process.env.OUTPUT_DIRECTORY, fi.sourceFile), function (err) {
+        if (err) {
+            throw err
+        } else {
+            console.log(`Successfully moved ${inputFile}`)
+        }
+      })
+    })
+  )
+}
